93d59bf258f6464fcae8b8cdd8a1f8da3a2e3653
Brian Chen <brian.chxn@gmail.com>
2015-04-18 18:05:18 +0800

    linewise and latin1 options

--- Main.hs
+++ Main.hs
@@ -1,11 +1,25 @@
 module Main (main) where
 
-import System.Environment
+import Control.Applicative ((<$>))
+import Control.Monad (when)
+import System.Environment (getArgs)
+import System.IO (hSetEncoding, stdin, latin1)
 import Text.Bcodex
+
+data Opts = Opts { isLinewise :: Bool, isLatin1 :: Bool, func :: String -> String }
+
+parseArgs :: [String] -> Either String Opts
+parseArgs ("all" : rargs)    = (\x -> x { isLinewise = False}) <$> parseArgs rargs
+parseArgs ("latin1" : rargs) = (\x -> x { isLatin1   = True }) <$> parseArgs rargs
+parseArgs rargs              = Opts True False <$> codex rargs
 
 main :: IO ()
 main = do
     args <- getArgs
-    case codex args of
+    case parseArgs args of
         Left em -> error em
-        Right f -> interact $ unlines . map f . lines
+        Right o -> do
+            when (isLatin1 o) $ hSetEncoding stdin latin1
+            let f = func o
+            interact $ if isLinewise o then unlines . map f . lines
+                                       else f
